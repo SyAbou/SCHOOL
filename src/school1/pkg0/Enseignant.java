6de9fa17f7c31c9c64e0491c4874e31d268cefbe
abou <abou@abou-PC>
2018-04-25 02:32:14 +0200

    initial commit

--- src/school1/pkg0/Enseignant.java
+++ src/school1/pkg0/Enseignant.java
@@ -49,20 +49,23 @@ Statement stm;
         jComboBox1 = new javax.swing.JComboBox<>();
         jLabel1 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("idEnseignant");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 44, 70, 20));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 160, 70, 20));
 
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("nomEnseignant");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 90, -1, -1));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 200, -1, -1));
 
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("prenomEnseignant");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, -1, -1));
-        getContentPane().add(nomENS, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 90, 100, -1));
-        getContentPane().add(prenomENS, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 130, 100, -1));
-        getContentPane().add(idENS, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 40, 100, -1));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, -1, -1));
+        getContentPane().add(nomENS, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 200, 100, -1));
+        getContentPane().add(prenomENS, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 240, 100, -1));
+        getContentPane().add(idENS, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 150, 100, -1));
 
         jButton5.setText("Enregistrer");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
@@ -70,15 +73,17 @@ Statement stm;
                 jButton5ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 240, -1, -1));
+        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 350, -1, -1));
 
         jComboBox1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jComboBox1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 180, 100, -1));
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 414, 300));
+        getContentPane().add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 290, 100, -1));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\abou\\Desktop\\ENSEI.png")); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 450));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
